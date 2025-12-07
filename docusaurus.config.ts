e3a3b8923825a6a4e2aa3e5e164441e75505fc52
Raja Mannan Khan <shabanajawed411@gmail.com>
2025-12-07 12:04:24 +0500

    URL_SETT

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://rajahadi.github.io',
+  url: 'https://physical-ai-humanoid-robotics-eta.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/-physical-ai-humanoid-robotics-book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
